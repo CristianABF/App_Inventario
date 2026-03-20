12d29515d7575db5e85a30b89fc104b9946fe9b6
CristianABF <cristianabf2002@gmail.com>
2026-03-20 10:40:00 -0300

    Cambios ligeros en la interfaz

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ async def main(page: ft.Page):
     page.window.height = 900
     page.window.maximized=True
     page.window.full_screen=False
-
     
     page.add(
         
--- variables.py
+++ variables.py
@@ -12,47 +12,59 @@ class buttons():
     '''Modulo de botones'''
     
     home_btn = ft.Button(
-            content='Inicio',
-            icon=ft.Icons.HOME,
-            style=ft.ButtonStyle(
-                color=ft.Colors.WHITE,
-                bgcolor=pred_color,
-                padding=20,
-                shape=ft.RoundedRectangleBorder(radius=0)
-            ),
-            height=pred_height,
-            width=pred_width
-        )
-    search_btn = ft.Button(
-            content='Buscar',
-            icon=ft.Icons.SEARCH,
-            style=ft.ButtonStyle(
-                color=ft.Colors.WHITE,
-                bgcolor=pred_color,
-                padding=20,
-                shape=ft.RoundedRectangleBorder(radius=0)
-            ),
-            height=pred_height,
-            width=pred_width
-        )
-    table_btn = ft.Button(
-            content='Tabla',
-            icon=ft.Icons.TABLE_CHART,
-            style=ft.ButtonStyle(
-                color=ft.Colors.WHITE,
-                bgcolor=pred_color,
-                padding=20,
-                shape=ft.RoundedRectangleBorder(radius=0)
-            ),
-            height=pred_height,
-            width=pred_width
-        )
+        content='Inicio',
+        icon=ft.Icons.HOME,
+        style=ft.ButtonStyle(
+            color=ft.Colors.WHITE,
+            bgcolor=pred_color,
+            padding=20,
+            shape=ft.RoundedRectangleBorder(radius=0)
+        ),
+        height=pred_height,
+        width=pred_width
+    )
+    users_btn = ft.Button(
+        content='Usuarios',
+        icon=ft.Icons.PEOPLE,
+        style=ft.ButtonStyle(
+            color=ft.Colors.WHITE,
+            bgcolor=pred_color,
+            padding=20,
+            shape=ft.RoundedRectangleBorder(radius=0)
+        ),
+        height=pred_height,
+        width=pred_width
+    )
+    products_btn = ft.Button(
+        content='Productos',
+        icon=ft.Icons.SHOPPING_CART,
+        style=ft.ButtonStyle(
+            color=ft.Colors.WHITE,
+            bgcolor=pred_color,
+            padding=20,
+            shape=ft.RoundedRectangleBorder(radius=0)
+        ),
+        height=pred_height,
+        width=pred_width
+    )
+    config_btn = ft.Button(
+        content='Config...',
+        icon=ft.CupertinoIcons.GEAR_ALT_FILL,
+        style=ft.ButtonStyle(
+            color=ft.Colors.WHITE,
+            bgcolor=pred_color,
+            padding=20,
+            shape=ft.RoundedRectangleBorder(radius=0)
+        ),
+        height=pred_height,
+        width=pred_width
+    )
 
 # IMAGENES
 class images():
     '''Modulo para incorporar imagenes'''
     
-    inventory_image = ft.Image(
+    inventory_icon = ft.Image(
         src='images/inventory.png', # Autor FREEPIC, from flaticon.com
         scale=0.7
     )
@@ -70,21 +82,21 @@ class pages():
                     bgcolor=ft.Colors.GREY_900,
                     border=ft.Border.all(2, pred_color),
                     padding=20,
-                    width=1772,
+                    width=1752,
                     height=1000,
                     border_radius=15,
                     alignment=ft.Alignment.CENTER
         )
     column_a=ft.Container(
-            content=ft.Column([images.inventory_image,buttons.home_btn,buttons.search_btn,buttons.table_btn,column_vertical_fill], spacing=0),
+            content=ft.Column([images.inventory_icon,buttons.home_btn,buttons.users_btn,buttons.products_btn,column_vertical_fill,buttons.config_btn], spacing=0),
             bgcolor="#1a1a1a",
-            width=120, #tamaño horizontal
+            width=140, #tamaño horizontal
             height=1000, #tamaño vertical
             border_radius=15,
             expand=False,
             
         )
-    # Contenedor que organiza todo en columnas |a|b|c|
+    # Contenedor que organiza todo en columnas |a|b|
     principal_page=ft.Row(
         controls=[
             # 1er Contenedor vertical (ocupa zona a)
